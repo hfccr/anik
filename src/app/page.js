c6ba57a5d08d35f27e1ef7142e6af0255f07d160
hfccr <hfccr@outlook.com>
2024-03-24 00:39:39 +0530

    Hero

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,9 @@ import { Stack, Typography } from "@mui/material";
 export default function Home() {
   return (
     <Stack direction="column" justifyContent="center" alignItems="center">
-      <Typography variant="h2">IPC Subnet Security By Restaking</Typography>
+      <Typography variant="h2">
+        Provide Trust To IPC Subnets With Restaking
+      </Typography>
     </Stack>
   );
 }
