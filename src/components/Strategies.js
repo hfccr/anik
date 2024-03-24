ae07d55d0498cc579cd1501f62e42668acae7146
hfccr <hfccr@outlook.com>
2024-03-24 23:40:49 +0530

    Move

--- src/components/Strategies.js
+++ src/components/Strategies.js
@@ -1,6 +1,7 @@
 "use client";
 import {
   Avatar,
+  Button,
   List,
   ListItem,
   ListItemIcon,
@@ -9,12 +10,22 @@ import {
 import React from "react";
 import Image from "next/image";
 import { strategies } from "@/util/strategies";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const Strategies = () => {
+  const pathname = usePathname();
   const items = strategies.map((strategy) => {
     const { key, title, ticker, logo } = strategy;
     return (
-      <ListItem key={key}>
+      <ListItem
+        key={key}
+        secondaryAction={
+          <Link href={`${pathname}/${key}`}>
+            <Button>Restake</Button>
+          </Link>
+        }
+      >
         <ListItemIcon>
           <Avatar sx={{ height: 36, width: 36 }}>
             <Image src={logo} width={36} height={36} alt={ticker} />
