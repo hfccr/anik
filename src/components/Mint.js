e6cb60598368c4eb04a597ceed275ec46c54c018
hfccr <hfccr@outlook.com>
2024-03-25 18:31:07 +0530

    Change button color

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -2,5 +2,9 @@
 import { Button } from "@mui/material";
 
 export const Mint = () => {
-  return <Button variant="outlined">Mint Test Tokens</Button>;
+  return (
+    <Button variant="outlined" color="secondary">
+      Mint Test Tokens
+    </Button>
+  );
 };
