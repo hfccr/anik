91ae0e1f1f66bc5940bedf004c1877b14ab71a4b
hfccr <hfccr@outlook.com>
2024-03-30 15:54:44 +0530

    Change images

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
 import { Box, Container, Divider, Stack, Typography } from "@mui/material";
 import Image from "next/image";
+import HubIcon from "@mui/icons-material/Hub";
+import SaveIcon from "@mui/icons-material/Save";
+import GetApp from "@mui/icons-material/GetApp";
 
 export default function Home() {
   return (
@@ -61,15 +64,18 @@ export default function Home() {
           direction={{ md: "column", lg: "row" }}
           spacing={5}
         >
-          <Image
-            src="/tree.svg"
-            alt="tree"
-            width={400}
-            height={400}
-            style={{ opacity: 0.4 }}
-          />
-          <Stack spacing={4}>
-            <Typography variant="h2">IPC Subnet Security</Typography>
+          <Image src="/flag.svg" alt="flag" width={400} height={400} />
+          <Stack spacing={4} sx={{ position: "relative" }}>
+            <Stack direction="row" alignItems="center" spacing={4}>
+              <HubIcon
+                sx={{
+                  height: 84,
+                  width: 84,
+                }}
+              />
+
+              <Typography variant="h2">IPC Subnet Security</Typography>
+            </Stack>
             <Typography variant="h6">
               Bootstrap trusted IPC subnets by delegation of Restaked LSTs to
               Subnet Validators
@@ -84,15 +90,17 @@ export default function Home() {
           direction={{ md: "column", lg: "row-reverse" }}
           spacing={5}
         >
-          <Image
-            src="/disks.svg"
-            alt="disks"
-            width={400}
-            height={400}
-            style={{ opacity: 0.6 }}
-          />
+          <Image src="/smoke.svg" alt="smoke" width={400} height={400} />
           <Stack spacing={4}>
-            <Typography variant="h2">Storage Providers</Typography>
+            <Stack direction="row" alignItems="center" spacing={4}>
+              <SaveIcon
+                sx={{
+                  height: 84,
+                  width: 84,
+                }}
+              />
+              <Typography variant="h2">Storage Providers</Typography>
+            </Stack>
             <Typography variant="h6">
               Help manage Storage Provider capital by delegating LSTs for deal
               collaterals
@@ -107,15 +115,17 @@ export default function Home() {
           direction={{ md: "column", lg: "row" }}
           spacing={5}
         >
-          <Image
-            src="/fetch.svg"
-            alt="fetch"
-            width={400}
-            height={400}
-            style={{ opacity: 0.6 }}
-          />
+          <Image src="/wave.svg" alt="wave" width={400} height={400} />
           <Stack spacing={4}>
-            <Typography variant="h2">Retrieval Protocols</Typography>
+            <Stack direction="row" alignItems="center" spacing={4}>
+              <GetApp
+                sx={{
+                  height: 84,
+                  width: 84,
+                }}
+              />
+              <Typography variant="h2">Retrieval Protocols</Typography>
+            </Stack>
             <Typography variant="h6">
               Delegate to offchain retrieval attestors & storage providers to
               secure the next generation of Retrieval Protocols
