2669095bda8173941ff57f3d2631a964e19dc45f
hfccr <hfccr@outlook.com>
2024-04-06 16:47:44 +0530

    Add ui for miner slasher flow

--- src/components/MinerSlashButton.js
+++ src/components/MinerSlashButton.js
@@ -1,5 +1,8 @@
 import { getSlasher } from "@/util/operatorTypes";
 import {
+  Alert,
+  AlertTitle,
+  Container,
   Button,
   Dialog,
   DialogTitle,
@@ -8,17 +11,29 @@ import {
   DialogContentText,
   Slide,
   Typography,
+  IconButton,
+  Stack,
+  Divider,
+  Paper,
+  useTheme,
 } from "@mui/material";
+import { Close } from "@mui/icons-material";
 import { useState, forwardRef } from "react";
 import toast from "react-hot-toast";
 import { useWriteContract } from "wagmi";
 import { minerSlasher } from "@/util/minerSlasher";
+import Image from "next/image";
 
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export const MinerSlashButton = ({ deal, operatorDetails }) => {
+  const theme = useTheme();
+  const imageSrc =
+    theme.palette.mode === "dark"
+      ? "/miner-slasher-flow.svg"
+      : "/miner-slasher-flow-light.svg";
   const { operatorAddress, operatorType } = operatorDetails;
   const { writeContract } = useWriteContract();
   const { minerId } = operatorDetails;
@@ -58,11 +73,69 @@ export const MinerSlashButton = ({ deal, operatorDetails }) => {
         onClose={handleClose}
         aria-describedby="slash-miner"
       >
-        <DialogTitle>Slash Miner t0{minerId.toString()}?</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            Miners behaving incorrectly will be slashed.
-          </DialogContentText>
+        <Stack direction="row" sx={{ padding: 2 }} spacing={2}>
+          <IconButton size="large" onClick={handleClose}>
+            <Close size="large" />
+          </IconButton>
+          <Typography variant="h3">
+            Slash Miner t0{minerId.toString()}
+          </Typography>
+        </Stack>
+        <Divider />
+        <DialogContent sx={{ paddingLeft: 6, paddingRight: 6 }}>
+          <Container maxWidth="lg">
+            <DialogContentText
+              id="alert-dialog-slide-description"
+              sx={{ padding: 2 }}
+            >
+              Slashing Storage Provider t0{minerId.toString()} For Deal{" "}
+              {deal.id}.
+            </DialogContentText>
+            <Stack
+              direction="row-reverse"
+              spacing={6}
+              justifyContent="space-between"
+            >
+              <Stack spacing={2} sx={{ paddingTop: 4 }}>
+                <DialogContentText id="alert-dialog-slide-description">
+                  * Storage Provider with terminated deals will be slashed by
+                  creating a slash request on Storage Provider Slasher Contract.
+                </DialogContentText>
+                <DialogContentText id="alert-dialog-slide-description">
+                  * Storage Provider Slasher contract will check the validity of
+                  the slash request by using the Market API to check if the deal
+                  is terminated and match the deal storage provider with the
+                  operator.
+                </DialogContentText>
+                <DialogContentText id="alert-dialog-slide-description">
+                  * Storage Provider Slasher will forward the slash request for
+                  the Operator to the Delegate Manager which handles shashing.
+                </DialogContentText>
+                <Alert severity="info">
+                  <AlertTitle>Slashing In Demo Mode</AlertTitle>
+                  Note that check for terminated deal by using Market API has
+                  been commented as of now to demonstrate Storage Provider
+                  slashing on Calibration
+                </Alert>
+              </Stack>
+              <Paper
+                variant="outlined"
+                sx={{
+                  padding: 4,
+                  flexGrow: 1,
+                }}
+              >
+                <Stack direction="column" spacing={2} justifyContent="center">
+                  <Image
+                    src={imageSrc}
+                    height={700}
+                    width={500}
+                    alt="ipc slasher flow"
+                  />
+                </Stack>
+              </Paper>
+            </Stack>
+          </Container>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} variant="outlined" color="secondary">
