46df6b5c4ea7b9de925bb48b80cdd50479a89e80
hfccr <hfccr@outlook.com>
2024-04-23 17:07:43 +0530

    Fix

--- src/app/next/page.js
+++ src/app/next/page.js
@@ -28,7 +28,8 @@ export default function Next() {
           The end goal is to connect Filecoin actors to native and ethereum
           restaking. Native restaking will enable fast paced development of
           Filecoin native use cases which will then be ported to Ethereum
-          restaking ecosystem via AVS development.
+          restaking ecosystem via AVS development which will unlock an
+          additional 15 Billion dollars of capital for the Filecoin ecosystem.
         </Typography>
         <Stack justifyContent="center" alignItems="center">
           <Image src={imageSrc} height={700} width={800} alt="next" />
